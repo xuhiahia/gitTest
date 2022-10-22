814a1df974776c6470a3d800e442da9c1bf2598b
Andy <Andy@atguigu.com>
2022-10-22 15:24:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/App.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
